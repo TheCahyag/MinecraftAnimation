0936e2dd55f1b5f88e0eee38c2ac23747a7097a6
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-07-20 23:11:37 -0400

    - Created constructor for Frame.java - Created methods in Frame.java to ensure readiness and created getters/setters

--- src/main/java/com/servegame/bl4de/Animation/models/Frame.java
+++ src/main/java/com/servegame/bl4de/Animation/models/Frame.java
@@ -7,14 +7,60 @@ import java.util.UUID;
  *
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
-public class Frame extends SubSpace3D{
-    private UUID owner;
+public class Frame extends SubSpace3D {
+    private UUID creator;
     private String name;
 
-
-    public Frame(UUID owner, int frames, String name){
-        super(); // Init the subspace
-
+    /**
+     * Frame constructor that defines the subspace
+     * @param creator player {@link UUID}
+     * @param name name of the frame
+     * @param subSpace {@link SubSpace3D}
+     */
+    public Frame(UUID creator, String name, SubSpace3D subSpace){
+        super(subSpace); // Init the subspace
+        this.creator = creator;
+        this.name = name;
     }
 
+    /**
+     * Ensure that the Frame's variables have been set
+     * @return boolean indicating complete initialization
+     */
+    public boolean isInitialized(){
+        if (this.getCornerOne() == null){
+            return false;
+        }
+        if (this.getCornerTwo() == null){
+            return false;
+        }
+        if (this.name.equals("")){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Getter for the {@link UUID} of the frame creator
+     * @return {@link UUID}
+     */
+    public UUID getCreator() {
+        return creator;
+    }
+
+    /**
+     * Getter for the name of the frame
+     * @return frame name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter for the frame name
+     * @param name frame name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 }
