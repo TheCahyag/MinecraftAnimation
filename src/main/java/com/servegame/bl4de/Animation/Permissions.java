c5b93255a82361d918d9935edb537477624535da
Brandon Bires-Navel <brn5915@rit.edu>
2017-07-22 14:52:25 -0400

    Added permissions for the base commands (still more to add)

--- src/main/java/com/servegame/bl4de/Animation/Permissions.java
+++ src/main/java/com/servegame/bl4de/Animation/Permissions.java
@@ -6,4 +6,17 @@ package com.servegame.bl4de.Animation;
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class Permissions {
+    public static final String ANIMATION_BASE       = "animate.base";
+    public static final String ANIMATION_CREATE     = "animate.create.base";
+    public static final String ANIMATION_DELETE     = "animate.delete.base";
+    public static final String ANIMATION_HELP       = "animate.help.base";
+    public static final String ANIMATION_LIST       = "animate.list.base";
+    public static final String ANIMATION_START      = "animate.start.base";
+    public static final String ANIMATION_STOP       = "animate.stop.base";
+    public static final String FRAME_CREATE         = "animate.frame.create.base";
+    public static final String FRAME_DELETE         = "animate.frame.delete.base";
+    public static final String FRAME_DISPLAY        = "animate.frame.display.base";
+    public static final String FRAME_DUPLICATE      = "animate.frame.duplicate.base";
+    public static final String FRAME_LIST           = "animate.frame.list.base";
+    public static final String FRAME_UPDATE         = "animate.frame.update.base";
 }
