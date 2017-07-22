eb150b290e38f11bc75a0e9bc64b718c70fbfd2f
Brandon Bires-Navel <brn5915@rit.edu>
2017-07-22 14:53:12 -0400

    Created exception for when a frame/animation is not properly initialized

---
+++ src/main/java/com/servegame/bl4de/Animation/exceptions/UninitializedException.java
@@ -0,0 +1,17 @@
+package com.servegame.bl4de.Animation.exceptions;
+
+/**
+ * File: UninitializedException.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class UninitializedException extends Exception {
+
+    /**
+     * UninitializedException
+     * @param detail Exception detail
+     */
+    public UninitializedException(String detail){
+        super(detail);
+    }
+}
