272ab45840f7660b95d59db88a72e467287579fb
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-01 22:45:38 -0400

    Changed response messages to instead use the static messages in TextResponses

--- src/main/java/com/servegame/bl4de/Animation/commands/animation/DeleteAnimation.java
+++ src/main/java/com/servegame/bl4de/Animation/commands/animation/DeleteAnimation.java
@@ -2,6 +2,7 @@ package com.servegame.bl4de.Animation.commands.animation;
 
 import com.servegame.bl4de.Animation.models.Animation;
 import com.servegame.bl4de.Animation.util.AnimationUtil;
+import com.servegame.bl4de.Animation.util.TextResponses;
 import com.servegame.bl4de.Animation.util.Util;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
@@ -11,7 +12,6 @@ import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.action.TextActions;
-import org.spongepowered.api.text.format.TextColors;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -25,31 +25,31 @@ public class DeleteAnimation implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         if (!(src instanceof Player)){
-            src.sendMessage(Text.of(TextColors.DARK_RED, "This command must be ran by a player in-game."));
+            src.sendMessage(TextResponses.PLAYER_ONLY_COMMAND_WARNING);
             return CommandResult.success();
         }
         Player player = (Player) src;
         UUID owner = player.getUniqueId();
         Optional<String> animationNameOptional = args.getOne("name");
         if (!animationNameOptional.isPresent()){
-            player.sendMessage(Text.of(TextColors.DARK_RED, "Animation name was not parsed correctly/wasn't specified."));
+            player.sendMessage(TextResponses.ANIMATION_NOT_SPECIFIED_ERROR);
             return CommandResult.empty();
         }
         String animationName = animationNameOptional.get();
         Optional<Animation> animationOptional = AnimationUtil.getAnimation(animationName, owner);
         if (!animationOptional.isPresent()){
             // Couldn't find the given animation
-            player.sendMessage(Text.of(TextColors.DARK_RED, "Animation was not found."));
+            player.sendMessage(TextResponses.ANIMATION_NOT_FOUND_ERROR);
             return CommandResult.empty();
         }
         if (!args.hasAny("f")){
             // Check for the -f
-            player.sendMessage(Text.of(Util.WARNING_COLOR, "If you sure you want to delete the '",
+            player.sendMessage(Text.of(Util.ERROR_COLOR, "If you sure you want to delete the '",
                     Util.NAME_COLOR, animationName,
-                    Util.WARNING_COLOR, "' animation, run",
+                    Util.ERROR_COLOR, "' animation, run",
                     Util.PRIMARY_COLOR, " /animation delete " + animationName,
                     Util.FLAG_COLOR, " -f").toBuilder()
-                    .append(Text.of(Util.WARNING_COLOR, Util.COMMAND_STYLE, ", or click this message."))
+                    .append(Text.of(Util.ERROR_COLOR, Util.COMMAND_STYLE, ", or click this message."))
                     .onClick(TextActions.runCommand("/animate delete " + animationName + " -f"))
                     .onHover(Util.COMMAND_HOVER)
                     .build());
@@ -66,7 +66,7 @@ public class DeleteAnimation implements CommandExecutor {
             return CommandResult.success();
         } else {
             // Animation was not deleted successfully
-            player.sendMessage(Text.of(TextColors.DARK_RED, "Animation could not be deleted properly."));
+            player.sendMessage(TextResponses.ANIMATION_DELETE_ERROR);
             return CommandResult.empty();
         }
     }
