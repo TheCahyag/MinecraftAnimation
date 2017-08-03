5bec03da506a707840817c76fc976b98bc9390db
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-02 22:25:37 -0400

    Removed implementation from a different branch

--- src/main/java/com/servegame/bl4de/Animation/commands/animation/BaseAnimation.java
+++ src/main/java/com/servegame/bl4de/Animation/commands/animation/BaseAnimation.java
@@ -15,6 +15,7 @@ import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 
+import java.util.Collection;
 import java.util.Optional;
 
 /**
@@ -70,75 +71,23 @@ public class BaseAnimation implements CommandExecutor {
         }
         Player player = (Player) src;
         Optional<Animation> animationOptional = AnimationUtil.getAnimation(animationName, player.getUniqueId());
-        Optional<String> argumentsOptional = args.getOne("remaining_arguments");
         if (!animationOptional.isPresent()){
             // Animation wasn't found
             player.sendMessage(TextResponses.ANIMATION_NOT_FOUND_ERROR);
             return CommandResult.success();
         }
-        if (!argumentsOptional.isPresent()){
-            // Command doesn't have enough arguments
-            player.sendMessage(TextResponses.GENERAL_ARGUMENTS_INCORRECT);
-            return CommandResult.empty();
-        }
         Animation animation = animationOptional.get();
-        String remainingArguments = argumentsOptional.get();
 
-        // Parse remaining command arguments
-        String[] tokens = remainingArguments.split(" ");
-
-        if (tokens.length == 0 || "info".equals(tokens[0].toLowerCase())){
-            // /animate <name> or /animate <name> info
-            // If the user doesn't specify a command after the name it defaults to info
-            CommandResult result = CommandResult.empty();
-            try {
-                result = new InfoAnimation(animation).execute(src, null);
-            } catch (CommandException e){
-                // What do I do with this? TODO
-                // When does this happen?
-                e.printStackTrace();
-            }
-            return result;
-        } else if (!"frame".equals(tokens[0].toLowerCase())){
-            // The command is not /animate <name> frame ... and therefore doesn't match any other commands
-            player.sendMessage(TextResponses.GENERAL_ARGUMENTS_INCORRECT);
-            return CommandResult.empty();
+        Optional<String> infoOptional = args.getOne("info");
+        Optional<String> frameOptional = args.getOne("frame");
+        if (infoOptional.isPresent()){
+            player.sendMessage(Text.of("we got the info", infoOptional.get()));
         }
-
-        // Parse arguments after the frame sub-command
-        String childCommand = tokens[1];
-        String argumentsToTheCommand = "";
-        for (int i = 2; i < tokens.length; i++) {
-            argumentsToTheCommand += tokens + " ";
+        if (frameOptional.isPresent()){
+            player.sendMessage(Text.of("we got the frame", frameOptional.get()));
         }
-        try {
-            switch (childCommand.toLowerCase()) {
-                case "create":
-                    // /animate <name> frame create <name> -h
-                    return new CreateFrame(animation).execute(src, CreateFrame.parseArguments(argumentsToTheCommand));
-                case "delete":
-                    // /animate <name> frame delete <name|num> -f
-                    return new DeleteFrame(animation).execute(src, DeleteFrame.parseArguments(argumentsToTheCommand));
-                case "display":
-                    // /animate <name> frame display <name|num>
-                    return new DisplayFrame(animation).execute(src, DisplayFrame.parseArguments(argumentsToTheCommand));
-                case "duplicate":
-                    // /animate <name> frame duplicate <name|num> [num]
-                    return new DuplicateFrame(animation).execute(src, DisplayFrame.parseArguments(argumentsToTheCommand));
-                case "list":
-                    // /animate <name> frame list
-                    return new ListFrames(animation).execute(src, DisplayFrame.parseArguments(argumentsToTheCommand));
-                case "update":
-                    // /animate <name> frame <name|num> -o
-                    return new UpdateFrame(animation).execute(src, DisplayFrame.parseArguments(argumentsToTheCommand));
-                default:
-                    player.sendMessage(TextResponses.GENERAL_ARGUMENTS_INCORRECT);
-                    return CommandResult.empty();
-            }
-        } catch (CommandException e){
-            // Again, what do I do with this? TODO
-            e.printStackTrace();
-            return CommandResult.empty();
-        }
+        player.sendMessage(Text.of("Le fin"));
+        // Parsing needed
+        return CommandResult.success();
     }
 }
