aa364b550dcbeb9d159d6af77d5b3187b1dd8f3e
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-03 22:01:22 -0400

    - Updated some messages to use TextResponses - The /animate list command will send a different message if there are no animations present for a given owner

--- src/main/java/com/servegame/bl4de/Animation/commands/animation/ListAnimation.java
+++ src/main/java/com/servegame/bl4de/Animation/commands/animation/ListAnimation.java
@@ -1,7 +1,7 @@
 package com.servegame.bl4de.Animation.commands.animation;
 
 import com.servegame.bl4de.Animation.util.AnimationUtil;
-import com.servegame.bl4de.Animation.util.Util;
+import com.servegame.bl4de.Animation.util.TextResponses;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -9,7 +9,8 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.format.TextColors;
+
+import static com.servegame.bl4de.Animation.util.Util.*;
 
 import java.util.ArrayList;
 
@@ -23,21 +24,27 @@ public class ListAnimation implements CommandExecutor {
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         // Check if the CommandSource is a player
         if (!(src instanceof Player)){
-            src.sendMessage(Text.of(TextColors.DARK_RED, "This command must be ran by a player in-game."));
+            src.sendMessage(TextResponses.PLAYER_ONLY_COMMAND_WARNING);
             return CommandResult.success();
         }
         Player player = (Player) src;
         ArrayList<String> animationsByOwner = AnimationUtil.getAnimationsByOwner(player.getUniqueId());
-        Text message = Text.builder()
-                .append(Text.of(Util.PRIMARY_COLOR, "Animations:\n"))
-                .build();
-        for (int i = 0; i < animationsByOwner.size(); i++) {
-            // Add all animations that are owned into a Text object
-            message = message.toBuilder()
-                    .append(Text.of(Util.NAME_COLOR, animationsByOwner.get(i) + "\n"))
+
+        if (animationsByOwner.size() != 0){
+            Text message = Text.builder()
+                    .append(Text.of(PRIMARY_COLOR, "Animations:\n"))
                     .build();
+            for (int i = 0; i < animationsByOwner.size(); i++) {
+                // Add all animations that are owned into a Text object
+                message = message.toBuilder()
+                        .append(Text.of(NAME_COLOR, animationsByOwner.get(i) + "\n"))
+                        .build();
+            }
+            player.sendMessage(message);
+        } else {
+            // There are no animations to display
+            player.sendMessage(Text.of(PRIMARY_COLOR, "There are no animations to display."));
         }
-        player.sendMessage(message);
         return CommandResult.success();
     }
 }
