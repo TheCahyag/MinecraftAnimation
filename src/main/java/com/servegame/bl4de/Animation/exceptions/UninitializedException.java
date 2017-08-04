899fd55bb08e29553fea6a39356e074e24b9ed52
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-03 22:05:16 -0400

    - Added a Text state and a constructor to go along with it

--- src/main/java/com/servegame/bl4de/Animation/exceptions/UninitializedException.java
+++ src/main/java/com/servegame/bl4de/Animation/exceptions/UninitializedException.java
@@ -1,5 +1,8 @@
 package com.servegame.bl4de.Animation.exceptions;
 
+import com.servegame.bl4de.Animation.util.Util;
+import org.spongepowered.api.text.Text;
+
 /**
  * File: UninitializedException.java
  *
@@ -7,11 +10,34 @@ package com.servegame.bl4de.Animation.exceptions;
  */
 public class UninitializedException extends Exception {
 
+    private Text detailText;
+
     /**
      * UninitializedException
      * @param detail Exception detail
      */
     public UninitializedException(String detail){
         super(detail);
+    }
+
+    /**
+     * Sets the message as a {@link Text} and also gives the
+     * super class a plain string to store
+     * @param detail {@link Text}
+     */
+    public UninitializedException(Text detail){
+        super(detail.toPlain());
+        this.detailText = detail;
+    }
+
+    /**
+     * Getter for the detail message
+     * @return {@link Text}
+     */
+    public Text getDetailText() {
+        if (detailText == null){
+            return Text.of(Util.ERROR_COLOR, super.getMessage());
+        }
+        return detailText;
     }
 }
