56af61de25d36d27e694765d4e1b1496f12e2f22
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-04 18:18:16 -0400

    Formatted the info message when called on a animation

--- src/main/java/com/servegame/bl4de/Animation/commands/animation/InfoAnimation.java
+++ src/main/java/com/servegame/bl4de/Animation/commands/animation/InfoAnimation.java
@@ -1,14 +1,24 @@
 package com.servegame.bl4de.Animation.commands.animation;
 
 import com.servegame.bl4de.Animation.models.Animation;
+import com.servegame.bl4de.Animation.util.TextResponses;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.service.user.UserStorageService;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import static com.servegame.bl4de.Animation.util.Util.*;
+import static org.spongepowered.api.text.format.TextColors.*;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
 
 /**
  * File: InfoAnimation.java
@@ -25,8 +35,51 @@ public class InfoAnimation implements CommandExecutor {
 
     @Override
     public CommandResult execute(CommandSource src, @Nullable CommandContext args) throws CommandException {
-        Text message = Text.builder().build();
-        src.sendMessage(Text.of("This animation name is: ", this.animation.getAnimationName()));
+        if (!(src instanceof Player)){
+            src.sendMessage(TextResponses.PLAYER_ONLY_COMMAND_WARNING);
+            return CommandResult.success();
+        }
+
+        // Get offline player
+        Optional<UserStorageService> userStorageServiceOptional = Sponge.getServiceManager().provide(UserStorageService.class);
+        if (!userStorageServiceOptional.isPresent()){
+            src.sendMessage(Text.of(ERROR_COLOR, "Uh oh, the UserStorageService.class couldn't be obtained..."));
+            return CommandResult.success();
+        }
+        UserStorageService userStorageService = userStorageServiceOptional.get();
+        Optional<Location<World>> cornerOneOptional = this.animation.getSubSpace().getCornerOne();
+        Optional<Location<World>> cornerTwoOptional = this.animation.getSubSpace().getCornerTwo();
+
+        // Get corner strings
+        String cornerOne, cornerTwo;
+        cornerOne = cornerOneOptional.map(Location::toString).orElse("nil");
+        cornerTwo = cornerTwoOptional.map(Location::toString).orElse("nil");
+
+        Text message = Text.builder()
+                .append(Text.of(SECONDARY_COLOR, "----------------------------------------------------\n",
+                        PRIMARY_COLOR, "Name",
+                        WHITE, ": ",
+                        NAME_COLOR, this.animation.getAnimationName() + "\n",
+                        PRIMARY_COLOR, "Owner",
+                        WHITE, ": ",
+                        NAME_COLOR, userStorageService.get(this.animation.getOwner()).get().getName() + "\n",
+                        PRIMARY_COLOR, "# of Frames",
+                        WHITE, ": ",
+                        SECONDARY_COLOR, this.animation.getNumOfFrames() + "\n",
+                        PRIMARY_COLOR, "Status",
+                        WHITE, ": ",
+                        SECONDARY_COLOR, this.animation.getStatus() + "\n",
+                        PRIMARY_COLOR, "Corner 1",
+                        WHITE, ": ",
+                        SECONDARY_COLOR, cornerOne + "\n",
+                        PRIMARY_COLOR, "Corner 2",
+                        WHITE, ": ",
+                        SECONDARY_COLOR, cornerTwo + "\n"))
+                .append(Text.of(SECONDARY_COLOR, "----------------------------------------------------\n"))
+                .append(getAnimationButtons(this.animation))
+                .append(Text.of(SECONDARY_COLOR, "----------------------------------------------------"))
+                .build();
+        src.sendMessage(message);
         return CommandResult.success();
     }
 }
