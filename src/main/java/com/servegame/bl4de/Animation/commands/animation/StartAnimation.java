57fa75640cf4bf57afc8ac85e9061b16d23ba34e
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-04 19:35:18 -0400

    Implemented the /animate <name> start command

--- src/main/java/com/servegame/bl4de/Animation/commands/animation/StartAnimation.java
+++ src/main/java/com/servegame/bl4de/Animation/commands/animation/StartAnimation.java
@@ -1,10 +1,16 @@
 package com.servegame.bl4de.Animation.commands.animation;
 
+import com.servegame.bl4de.Animation.models.Animation;
+import com.servegame.bl4de.Animation.util.AnimationUtil;
+import com.servegame.bl4de.Animation.util.TextResponses;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+
+import java.util.Optional;
 
 /**
  * File: StartAnimation.java
@@ -14,6 +20,38 @@ import org.spongepowered.api.command.spec.CommandExecutor;
 public class StartAnimation implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        return null;
+        if (!(src instanceof Player)){
+            src.sendMessage(TextResponses.PLAYER_ONLY_COMMAND_WARNING);
+            return CommandResult.success();
+        }
+        Player player = ((Player) src);
+
+        // Get animation
+        Optional<String> animationNameOptional = args.getOne("animation_name");
+        if (!animationNameOptional.isPresent()){
+            player.sendMessage(TextResponses.ANIMATION_NOT_SPECIFIED_ERROR);
+            return CommandResult.empty();
+        }
+        Optional<Animation> animationOptional = AnimationUtil.getAnimation(animationNameOptional.get(), player.getUniqueId());
+        if (!animationOptional.isPresent()){
+            player.sendMessage(TextResponses.ANIMATION_NOT_FOUND_ERROR);
+            return CommandResult.success();
+        }
+        Animation animation = animationOptional.get();
+
+        // Parse flag arguments
+        Optional<Integer> frameToStartOnOptional = args.getOne("frame");
+        Optional<Integer> tickDelayOptional = args.getOne("delay");
+        Optional<Integer> cyclesOptional = args.getOne("cycles");
+        tickDelayOptional.ifPresent(animation::setTickDelay);
+        cyclesOptional.ifPresent(animation::setCycles);
+
+        if (frameToStartOnOptional.isPresent()){
+            animation.start(frameToStartOnOptional.get());
+        } else {
+            animation.start();
+        }
+        return CommandResult.success();
+
     }
 }
