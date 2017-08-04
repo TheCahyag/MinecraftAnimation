13cad426c9b24be0db50f1427d1a5dabf4410fd6
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-03 21:28:48 -0400

    Added Text messages and used a static import for Util

--- src/main/java/com/servegame/bl4de/Animation/util/TextResponses.java
+++ src/main/java/com/servegame/bl4de/Animation/util/TextResponses.java
@@ -1,6 +1,6 @@
 package com.servegame.bl4de.Animation.util;
 
-import org.spongepowered.api.command.CommandSource;
+import static com.servegame.bl4de.Animation.util.Util.*;
 import org.spongepowered.api.text.Text;
 
 /**
@@ -12,19 +12,20 @@ public class TextResponses {
 
     // General Responses
     // General Warning Responses
-    public static final Text PLAYER_ONLY_COMMAND_WARNING = Text.of(Util.WARNING_COLOR, "This command must be ran by a player in-game.");
+    public static final Text PLAYER_ONLY_COMMAND_WARNING = Text.of(WARNING_COLOR, "This command must be ran by a player in-game.");
 
     // General Error Responses
-    public static final Text GENERAL_ARGUMENTS_INCORRECT = Text.of(Util.ERROR_COLOR, "The arguments supplied are incorrect or were not parsed correctly.");
+    public static final Text GENERAL_ARGUMENTS_INCORRECT = Text.of(ERROR_COLOR, "The arguments supplied are incorrect or were not parsed correctly.");
 
     // Animation Responses
     // Animation Warning Responses
     // Animation Error Responses
-    public static final Text ANIMATION_SAVE_ERROR = Text.of(Util.ERROR_COLOR, "There was a problem saving the animation.");
-    public static final Text ANIMATION_DELETE_ERROR = Text.of(Util.ERROR_COLOR, "There was a problem deleting the animation.");
-    public static final Text ANIMATION_ALREADY_EXISTS_ERROR = Text.of(Util.ERROR_COLOR, "The specified animation already exists.");
-    public static final Text ANIMATION_NOT_FOUND_ERROR = Text.of(Util.ERROR_COLOR, "Animation was not found.");
-    public static final Text ANIMATION_NOT_SPECIFIED_ERROR = Text.of(Util.ERROR_COLOR, "Animation name was not parsed correctly/wasn't specified.");
+    public static final Text ANIMATION_SAVE_ERROR = Text.of(ERROR_COLOR, "There was a problem saving the animation.");
+    public static final Text ANIMATION_DELETE_ERROR = Text.of(ERROR_COLOR, "There was a problem deleting the animation.");
+    public static final Text ANIMATION_ALREADY_EXISTS_ERROR = Text.of(ERROR_COLOR, "The specified animation already exists.");
+    public static final Text ANIMATION_NOT_FOUND_ERROR = Text.of(ERROR_COLOR, "Animation was not found.");
+    public static final Text ANIMATION_NOT_SPECIFIED_ERROR = Text.of(ERROR_COLOR, "Animation name was not parsed correctly/wasn't specified.");
+    public static final Text ANIMATION_NOT_INITIALIZED_ERROR = Text.of(ERROR_COLOR, "The animation is currently not fully initialized.");
 
     // Frame Responses
     // Frame Warning Responses
