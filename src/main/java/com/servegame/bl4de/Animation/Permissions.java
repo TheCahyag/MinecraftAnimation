f2f4b645e495a8ffbad23faf29a3d5beb7d0df70
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-04 19:23:30 -0400

    Added flag based permissions

--- src/main/java/com/servegame/bl4de/Animation/Permissions.java
+++ src/main/java/com/servegame/bl4de/Animation/Permissions.java
@@ -8,12 +8,16 @@ package com.servegame.bl4de.Animation;
 public class Permissions {
     public static final String ANIMATION_BASE       = "animate.base";
     public static final String ANIMATION_CREATE     = "animate.create.base";
+    public static final String ANIMATION_CREATE_FLAG_F     = "animate.create.flag.frame";
+    public static final String ANIMATION_CREATE_FLAG_D     = "animate.create.flag.delay";
+    public static final String ANIMATION_CREATE_FLAG_C     = "animate.create.flag.cycles";
     public static final String ANIMATION_DELETE     = "animate.delete.base";
     public static final String ANIMATION_INFO       = "animate.info.base";
     public static final String ANIMATION_HELP       = "animate.help.base";
     public static final String ANIMATION_LIST       = "animate.list.base";
     public static final String ANIMATION_START      = "animate.start.base";
     public static final String ANIMATION_STOP       = "animate.stop.base";
+    public static final String ANIMATION_PAUSE      = "animate.pause.base";
     public static final String FRAME_CREATE         = "animate.frame.create.base";
     public static final String FRAME_DELETE         = "animate.frame.delete.base";
     public static final String FRAME_DISPLAY        = "animate.frame.display.base";
