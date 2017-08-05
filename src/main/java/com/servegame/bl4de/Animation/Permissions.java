2b8e86d428b71fba911f6c9bbe6b76e8a1a88783
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-04 23:10:12 -0400

    Added some permissions

--- src/main/java/com/servegame/bl4de/Animation/Permissions.java
+++ src/main/java/com/servegame/bl4de/Animation/Permissions.java
@@ -6,22 +6,26 @@ package com.servegame.bl4de.Animation;
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class Permissions {
-    public static final String ANIMATION_BASE       = "animate.base";
-    public static final String ANIMATION_CREATE     = "animate.create.base";
-    public static final String ANIMATION_CREATE_FLAG_F     = "animate.create.flag.frame";
-    public static final String ANIMATION_CREATE_FLAG_D     = "animate.create.flag.delay";
-    public static final String ANIMATION_CREATE_FLAG_C     = "animate.create.flag.cycles";
-    public static final String ANIMATION_DELETE     = "animate.delete.base";
-    public static final String ANIMATION_INFO       = "animate.info.base";
-    public static final String ANIMATION_HELP       = "animate.help.base";
-    public static final String ANIMATION_LIST       = "animate.list.base";
-    public static final String ANIMATION_START      = "animate.start.base";
-    public static final String ANIMATION_STOP       = "animate.stop.base";
-    public static final String ANIMATION_PAUSE      = "animate.pause.base";
-    public static final String FRAME_CREATE         = "animate.frame.create.base";
-    public static final String FRAME_DELETE         = "animate.frame.delete.base";
-    public static final String FRAME_DISPLAY        = "animate.frame.display.base";
-    public static final String FRAME_DUPLICATE      = "animate.frame.duplicate.base";
-    public static final String FRAME_LIST           = "animate.frame.list.base";
-    public static final String FRAME_UPDATE         = "animate.frame.update.base";
+    public static final String ANIMATION_BASE               = "animate.base";
+    public static final String ANIMATION_CREATE             = "animate.create.base";
+    public static final String ANIMATION_DELETE             = "animate.delete.base";
+    public static final String ANIMATION_INFO               = "animate.info.base";
+    public static final String ANIMATION_HELP               = "animate.help.base";
+    public static final String ANIMATION_LIST               = "animate.list.base";
+    public static final String ANIMATION_START              = "animate.start.base";
+    public static final String ANIMATION_START_FLAG_F       = "animate.start.flag.frame";
+    public static final String ANIMATION_START_FLAG_D       = "animate.start.flag.delay";
+    public static final String ANIMATION_START_FLAG_C       = "animate.start.flag.cycles";
+    public static final String ANIMATION_STOP               = "animate.stop.base";
+    public static final String ANIMATION_PAUSE              = "animate.pause.base";
+    public static final String ANIMATION_SET_NAME           = "animate.set.name";
+    public static final String ANIMATION_SET_POS            = "animate.set.pos";
+
+    public static final String FRAME_CREATE                 = "animate.frame.create.base";
+    public static final String FRAME_DELETE                 = "animate.frame.delete.base";
+    public static final String FRAME_DISPLAY                = "animate.frame.display.base";
+    public static final String FRAME_DUPLICATE              = "animate.frame.duplicate.base";
+    public static final String FRAME_LIST                   = "animate.frame.list.base";
+    public static final String FRAME_UPDATE                 = "animate.frame.update.base";
+
 }
