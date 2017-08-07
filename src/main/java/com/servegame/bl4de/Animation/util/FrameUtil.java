368f2e5becf9d5d9afca06ccea5a179eecc350a2
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-07 18:26:44 -0400

    Added method javadoc

--- src/main/java/com/servegame/bl4de/Animation/util/FrameUtil.java
+++ src/main/java/com/servegame/bl4de/Animation/util/FrameUtil.java
@@ -13,6 +13,17 @@ import java.util.Random;
  */
 public class FrameUtil {
 
+    /**
+     * Generates a name for a frame by starting with the string frame0,
+     * checking if that exists and if so will check for frame1 and so
+     * on until there is an availability
+     *
+     * TODO:
+     * May need to refactor this to remember the last number it was
+     * on to reduce redundant code execution
+     * @param animation the {@link Animation} the {@link Frame} will belong to
+     * @return String - name for the frame
+     */
     public static String generateFrameName(Animation animation){
         int counter = 0;
         List<Frame> frames = animation.getFrames();
