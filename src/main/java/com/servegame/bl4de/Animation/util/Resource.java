a6fc32f1b7581a8195f116c4f4d63e385a219b2b
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-10 17:39:58 -0400

    Created Resource.java to contain information for the @Plugin annotation in AnimationPlugin.java

---
+++ src/main/java/com/servegame/bl4de/Animation/util/Resource.java
@@ -0,0 +1,16 @@
+package com.servegame.bl4de.Animation.util;
+
+/**
+ * File: Resource.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class Resource {
+
+    public final static String NAME = "Animation";
+    public final static String ID = "animation";
+    public final static String VERSION = "0.0.0";
+    public final static String AUTHORS = "TheCahyag";
+    public final static String URL = "https://github.com/TheCahyag/animation";
+
+}
