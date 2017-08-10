c0f75156db5ec819dad6a7633979d7a1f3c4b76b
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-08-09 23:25:58 -0400

    Implemented /animate <name> frame list command, probably will need to refactor this later on.

--- src/main/java/com/servegame/bl4de/Animation/command/frame/ListFrames.java
+++ src/main/java/com/servegame/bl4de/Animation/command/frame/ListFrames.java
@@ -1,12 +1,20 @@
 package com.servegame.bl4de.Animation.command.frame;
 
 import com.servegame.bl4de.Animation.models.Animation;
+import com.servegame.bl4de.Animation.models.Frame;
+import com.servegame.bl4de.Animation.util.AnimationUtil;
+import com.servegame.bl4de.Animation.util.FrameUtil;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.text.Text;
+
+import java.util.ArrayList;
+
+import static com.servegame.bl4de.Animation.util.Util.*;
+import static org.spongepowered.api.text.format.TextColors.*;
 
 /**
  * File: ListFrames.java
@@ -21,18 +29,40 @@ public class ListFrames implements CommandExecutor {
         this.animation = animation;
     }
 
-    /**
-     * Takes a string and will parse the arguments
-     * @param rawString
-     * @return
-     */
-    public static CommandContext parseArguments(String rawString){
-        return null;
-    }
-
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        src.sendMessage(Text.of("Hello from listframe"));
+
+        // Created header for the frame list
+        Text message = Text.builder()
+                .append(Text.of(PRIMARY_COLOR, "Animation",
+                        WHITE, ": "))
+                .append(AnimationUtil.linkToAnimationInfo(animation))
+                .append(Text.of(PRIMARY_COLOR, "\n# of frames",
+                        WHITE, ": ",
+                        SECONDARY_COLOR, this.animation.getNumOfFrames() + "\n",
+                        PRIMARY_COLOR, "Frames",
+                        WHITE, ":\n"))
+                .build();
+
+        // List all the frames
+        ArrayList<Frame> frames = this.animation.getFrames();
+        for (int i = 0; i < this.animation.getNumOfFrames(); i++) {
+            message = Text.builder()
+                    .append(Text.of(PRIMARY_COLOR, i,
+                            WHITE, ": "))
+                    .append(FrameUtil.linkToFrameInfo(frames.get(i), animation))
+                    .append(Text.of("\n"))
+                    .build();
+        }
+
+        // Add buttons for the display
+        message = message.toBuilder()
+                .append(Text.of(SECONDARY_COLOR, "----------------------------------------------------\n"))
+                .append(FrameUtil.getButtonsForList(animation))
+                .append(Text.of(SECONDARY_COLOR, "----------------------------------------------------"))
+                .build();
+
+        src.sendMessage(message);
         return CommandResult.success();
     }
 }
