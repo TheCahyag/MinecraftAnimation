0eb9783ec0c37ef4e5e4dc80ac837ed4354c33b3
Brandon Bires-Navel <brn5915@rit.edu>
2017-08-21 07:10:07 -0400

    Added permission for toggling debug status

--- src/main/java/com/servegame/bl4de/Animation/Permissions.java
+++ src/main/java/com/servegame/bl4de/Animation/Permissions.java
@@ -35,5 +35,6 @@ public class Permissions {
     public static final String STOP_ALL_ANIMATIONS          = "animate.admin.stopall";
     public static final String LIST_RUNNING_ANIMAITONS      = "animate.admin.listall";
     public static final String FORCE_STOP_ANIMATION         = "animate.admin.forcestop";
+    public static final String TOGGLE_DEBUG                 = "animate.admin.debug";
 
 }
