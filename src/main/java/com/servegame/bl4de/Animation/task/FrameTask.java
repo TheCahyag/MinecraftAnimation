6900f5c8bf4dd4e7990418c1ffbf6c7904d1ae6e
Brandon Bires-Navel <brn5915@rit.edu>
2017-08-21 00:32:05 -0400

    Added debugInfo method

--- src/main/java/com/servegame/bl4de/Animation/task/FrameTask.java
+++ src/main/java/com/servegame/bl4de/Animation/task/FrameTask.java
@@ -1,8 +1,10 @@
 package com.servegame.bl4de.Animation.task;
 
+import com.servegame.bl4de.Animation.AnimationPlugin;
 import com.servegame.bl4de.Animation.model.Frame;
 import com.servegame.bl4de.Animation.model.SubSpace3D;
 import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.world.Location;
 
 /**
@@ -21,4 +23,14 @@ public interface FrameTask extends Runnable {
      * blocks for a given {@link Frame}.
      */
     void modifyWorld();
+
+    /**
+     * debugInfo is a helper method that will print relevant debug
+     * information when the {@link FrameDisplayTask#run()} method
+     * is executed. This method is only called if the
+     * {@link AnimationPlugin#isDebug()} returns true. Returning
+     * true would indicate that the {@link User} ran the
+     * "/animate debug" command.
+     */
+    void debugInfo();
 }
