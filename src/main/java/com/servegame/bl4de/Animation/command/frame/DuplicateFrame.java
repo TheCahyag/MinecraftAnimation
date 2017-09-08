2c37a7f2b2049b44d2926760261dce8c92346577
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-09-07 23:17:42 -0400

    Implemented the command side to /animate <name> frame duplicate <name|num> [num], still need to implement the backend for this

--- src/main/java/com/servegame/bl4de/Animation/command/frame/DuplicateFrame.java
+++ src/main/java/com/servegame/bl4de/Animation/command/frame/DuplicateFrame.java
@@ -1,5 +1,8 @@
 package com.servegame.bl4de.Animation.command.frame;
 
+import com.servegame.bl4de.Animation.controller.AnimationController;
+import com.servegame.bl4de.Animation.controller.FrameController;
+import com.servegame.bl4de.Animation.exception.UninitializedException;
 import com.servegame.bl4de.Animation.model.Animation;
 import com.servegame.bl4de.Animation.model.Frame;
 import com.servegame.bl4de.Animation.util.TextResponses;
@@ -12,6 +15,8 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
 
 import java.util.Optional;
+
+import static com.servegame.bl4de.Animation.util.Util.*;
 
 /**
  * File: DuplicateFrame.java
@@ -33,17 +38,85 @@ public class DuplicateFrame implements CommandExecutor {
             return CommandResult.success();
         }
         Player player = ((Player) src);
-        Optional<String> frameNameOptional = args.getOne("frame_name");
-        Frame frame;
-
         if (this.animation.isRunning()){
             player.sendMessage(TextResponses.ANIMATION_CANT_BE_RUNNING);
             return CommandResult.success();
         }
 
+        Optional<String> frameNameOptional = args.getOne("frame_name_num");
+        if (!frameNameOptional.isPresent()){
+            // Frame not specified
+            player.sendMessage(TextResponses.FRAME_NOT_SPECIFIED_ERROR);
+            return CommandResult.empty();
+        }
+        String frameName = frameNameOptional.get();
+        Optional<Frame> frameOptional;
 
+        if (isNumeric(frameName)){
+            // User specified a frame number 0, 1, ..., n
+            frameOptional = this.animation.getFrame(Integer.parseInt(frameName));
+        } else {
+            // User specified a frame name
+            frameOptional = this.animation.getFrame(frameName);
+        }
 
-        src.sendMessage(Text.of("Hello from duplicateframe"));
+        if (!frameOptional.isPresent()){
+            // Couldn't find a frame by the given name
+            player.sendMessage(TextResponses.FRAME_NOT_FOUND_ERROR);
+            return CommandResult.empty();
+        }
+        Frame theFrame = frameOptional.get(), newFrame = null;
+        boolean frameInserted = false;
+        int insertIndex = 0;
+        try {
+            newFrame = (Frame) frameOptional.get().clone();
+            newFrame.setName(FrameController.generateFrameName(this.animation));
+            Optional<String> optionalNum = args.getOne("num");
+            if (optionalNum.isPresent()){
+                // The user specified where to put the frame
+                insertIndex = Integer.parseInt(optionalNum.get());
+                this.animation.addFrame(newFrame, insertIndex);
+                frameInserted = true;
+            } else {
+                // The user didn't specify a location to put the cloned frame
+                int indexOfCloneSource = this.animation.getFrames().indexOf(theFrame);
+                insertIndex = indexOfCloneSource + 1;
+                this.animation.addFrame(newFrame, insertIndex);
+                frameInserted = true;
+            }
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            player.sendMessage(Text.of(ERROR_COLOR, "Something has gone horribly wrong..."));
+        } catch (UninitializedException e){
+            player.sendMessage(TextResponses.FRAME_NOT_INITIALIZED_ERROR);
+        } catch (NumberFormatException e){
+            player.sendMessage(TextResponses.NUMBER_FORMAT_EXCEPTION);
+        }
+        if (frameInserted){
+            // Frame went in just fine
+            if (AnimationController.saveAnimation(this.animation)){
+                Text message = Text.of(
+                        PRIMARY_COLOR, "Frame '",
+                        NAME_COLOR, theFrame.getName(),
+                        PRIMARY_COLOR, "' ",
+                        ACTION_COLOR, "duplicated",
+                        PRIMARY_COLOR, " successfully. ",
+                        ACTION_COLOR, "Inserted ",
+                        PRIMARY_COLOR, "'",
+                        NAME_COLOR, newFrame.getName(),
+                        PRIMARY_COLOR, "' at index " + insertIndex + "."
+                );
+                player.sendMessage(message);
+            } else {
+                // The animation failed to save
+                player.sendMessage(TextResponses.ANIMATION_SAVE_ERROR);
+                return CommandResult.empty();
+            }
+        } else {
+            // The frame for whatever reason was never inserted
+            player.sendMessage(TextResponses.FRAME_FAILED_DUPLICATION_ERROR);
+            return CommandResult.empty();
+        }
         return CommandResult.success();
     }
 }
