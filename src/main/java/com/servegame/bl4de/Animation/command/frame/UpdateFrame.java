91a82199d8f8b317d906225558ef319a74126b49
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-09-11 22:25:40 -0400

    Implementation of /animate <name> frame update <frame_name_num>

--- src/main/java/com/servegame/bl4de/Animation/command/frame/UpdateFrame.java
+++ src/main/java/com/servegame/bl4de/Animation/command/frame/UpdateFrame.java
@@ -1,8 +1,10 @@
 package com.servegame.bl4de.Animation.command.frame;
 
+import com.servegame.bl4de.Animation.controller.AnimationController;
 import com.servegame.bl4de.Animation.model.Animation;
 import com.servegame.bl4de.Animation.model.Frame;
 import com.servegame.bl4de.Animation.util.TextResponses;
+import com.servegame.bl4de.Animation.util.Util;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -33,14 +35,54 @@ public class UpdateFrame implements CommandExecutor {
             return CommandResult.success();
         }
         Player player = ((Player) src);
-        Optional<String> frameNameOptional = args.getOne("frame_name");
-        Frame frame;
-
         if (this.animation.isRunning()){
             player.sendMessage(TextResponses.ANIMATION_CANT_BE_RUNNING);
             return CommandResult.success();
         }
-        src.sendMessage(Text.of("Hello from updateframe"));
 
-        return CommandResult.success();    }
+        // Get the frame
+        Optional<String> frameNameOptional = args.getOne("frame_name_num");
+        if (!frameNameOptional.isPresent()){
+            // No frame name
+            player.sendMessage(TextResponses.FRAME_NOT_SPECIFIED_ERROR);
+            return CommandResult.empty();
+        }
+        Optional<Frame> frameOptional;
+        String frameNameNum = frameNameOptional.get();
+        if (Util.isNumeric(frameNameNum)){
+            frameOptional = this.animation.getFrame(Integer.parseInt(frameNameNum));
+        } else {
+            frameOptional = this.animation.getFrame(frameNameNum);
+        }
+
+        if (!frameOptional.isPresent()){
+            // The frame wasn't found in this animation
+            player.sendMessage(TextResponses.FRAME_NOT_FOUND_ERROR);
+            return CommandResult.empty();
+        }
+
+        Frame frame = frameOptional.get();
+
+//        int frameIndex = this.animation.getIndexOfFrame(frame);
+
+        frame.setContents(Util.copyWorldToSubSpace(frame.getCornerOne().get(), frame.getCornerTwo().get()));
+
+        System.out.println(this.animation.getFrame(frame.getName()));
+
+        // Save the Animation
+        if (AnimationController.saveAnimation(this.animation)){
+            // Animation was created and saved successfully
+            player.sendMessage(Text.of(Util.PRIMARY_COLOR, "Frame ",
+                    Util.PRIMARY_COLOR, "'",
+                    Util.NAME_COLOR, frame.getName(),
+                    Util.PRIMARY_COLOR, "' ",
+                    Util.ACTION_COLOR, "updated ",
+                    Util.PRIMARY_COLOR, "successfully."));
+            return CommandResult.success();
+        } else {
+            // There was a problem creating the animation
+            player.sendMessage(TextResponses.ANIMATION_SAVE_ERROR);
+            return CommandResult.empty();
+        }
+    }
 }
