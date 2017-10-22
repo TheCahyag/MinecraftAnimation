dd9537266ccf1905ee55732c59b3176e7586e6b2
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-10-21 23:53:37 -0400

    - Update message in class regarded WIP /animate stats command

--- src/main/java/com/servegame/bl4de/Animation/data/AnimationStatistics.java
+++ src/main/java/com/servegame/bl4de/Animation/data/AnimationStatistics.java
@@ -9,6 +9,11 @@ public class AnimationStatistics {
     public static int ANIMATIONS_RUNNING = 0;
     public static int ANIMATIONS_TOTAL;
 
+    /* This class and the entire /animate stats command is a WIP. It's currently
+     * just on the backlog and haven't really started too much on this specific
+     * feature. If you have any wants add it on the github page (the enhancement post)
+     */
+
 
 //    public static int getAnimationsTotal(){
 //        AnimationController.getAnimations()
