809e56de46cf8b99c37712255a54a2cfd08904d9
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-12-15 01:50:28 -0500

    Added Resource file to contain constants that pertain to the database

---
+++ src/main/java/com/servegame/bl4de/Animation/data/SQLResources.java
@@ -0,0 +1,12 @@
+package com.servegame.bl4de.Animation.data;
+
+/**
+ * File: SQLResources.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class SQLResources {
+    public static final String ANIMATION_TABLE = "ANIMATION_TABLE";
+
+    public static final String[] TABLES = {ANIMATION_TABLE};
+}
