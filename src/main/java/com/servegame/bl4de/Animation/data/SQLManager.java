2dd4b4c3ee449894a0fbb8296af3cc0b8645cf71
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-12-15 01:49:38 -0500

    Implemented methods to create and drop frame tables

--- src/main/java/com/servegame/bl4de/Animation/data/SQLManager.java
+++ src/main/java/com/servegame/bl4de/Animation/data/SQLManager.java
@@ -10,6 +10,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static com.servegame.bl4de.Animation.data.SQLResources.ANIMATION_TABLE;
+import static com.servegame.bl4de.Animation.data.SQLResources.TABLES;
+
 /**
  * File: SQLManager.java
  *
@@ -20,10 +23,14 @@ public class SQLManager {
 
     private PluginContainer plugin;
     private String database;
+
+    /*
     private String url;
     private String prefix;
     private String username;
     private String password;
+    */
+
 
     /**
      * TODO
@@ -36,19 +43,55 @@ public class SQLManager {
     }
 
     /**
-     * TODO
+     * Create default must-have tables
      */
     private void initSettings(){
         // In the future this would get data from a config file
         this.database = "ANIMATIONS";
         try (Connection connection = getDataSource().getConnection()){
-            connection.prepareStatement("CREATE TABLE IF NOT EXISTS ANIMATION_TABLE (name VARCHAR2(255), owner UUID, data VARCHAR2(" + Long.MAX_VALUE + "))")
-                    .executeUpdate();
-            connection.commit();
-            connection.close();
+            for (String table :
+                    TABLES) {
+                connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + table + " (name VARCHAR2(255), owner UUID, data CLOB)")
+                        .executeUpdate();
+
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Create frame table, usually called when an Animation is created
+     * @param name - name of the table
+     * @return true if successful, false otherwise
+     */
+    protected boolean createFrameTable(String name){
+        try (Connection conn = getConnection()){
+            conn.prepareStatement("CREATE TABLE " + name + " (id PRIMARY KEY VARCHAR2(255), data CLOB)").executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Drop the frame table, usually called when an animation is deleted
+     * @param name - name of the table
+     * @return true if it was successful, false otherwise
+     */
+    protected boolean dropFrameTable(String name){
+        if (name.equals(ANIMATION_TABLE)){
+            // Can't drop animations table
+            return false;
+        }
+        try (Connection conn = getConnection()){
+            conn.prepareStatement("DROP TABLE " + name).executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     /**
