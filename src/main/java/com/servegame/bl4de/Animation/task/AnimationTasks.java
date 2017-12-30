c47bbfcf681ab7490e4276282379298b23c80d3c
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-12-30 08:49:32 -0500

    Changed the way tasks are constructed, this fixed the bug where frames will become out of sync after time. The tasks are given a interval delay in ticks now rather then milliseconds before.

--- src/main/java/com/servegame/bl4de/Animation/task/AnimationTasks.java
+++ src/main/java/com/servegame/bl4de/Animation/task/AnimationTasks.java
@@ -4,13 +4,10 @@ import com.servegame.bl4de.Animation.AnimationPlugin;
 import com.servegame.bl4de.Animation.model.Animation;
 import com.servegame.bl4de.Animation.model.Frame;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.scheduler.Scheduler;
-import org.spongepowered.api.scheduler.SpongeExecutorService;
 import org.spongepowered.api.scheduler.Task;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * File: AnimationTasks.java
@@ -22,15 +19,13 @@ public class AnimationTasks {
 
     private Animation animation;
     private List<Task> tasks;
-    private SpongeExecutorService syncExecutorService;
 
     /**
      * Constructor for AnimationTasks
      * @param animation - the {@link Animation}
      */
-    public AnimationTasks(Animation animation, SpongeExecutorService spongeExecutorService){
+    public AnimationTasks(Animation animation){
         this.animation = animation;
-        this.syncExecutorService = spongeExecutorService;
         this.tasks = new ArrayList<>();
     }
 
@@ -49,12 +44,11 @@ public class AnimationTasks {
                     new FrameDisplayTask(frame, this.animation.getTickDelay(), i);
 
             // Create a task for each frame giving proper delay times
-            SpongeExecutorService.SpongeFuture tmp = syncExecutorService.scheduleAtFixedRate(
-                    frameDisplayTask,
-                    ticksToMilliseconds(initDelay),
-                    ticksToMilliseconds(periodBetweenRepeat),
-                    TimeUnit.MILLISECONDS);
-            this.tasks.add(tmp.getTask());
+            Task.Builder taskBuilder = Task.builder()
+                    .delayTicks(initDelay)
+                    .intervalTicks(periodBetweenRepeat)
+                    .execute(frameDisplayTask);
+            this.tasks.add(taskBuilder.submit(AnimationPlugin.plugin));
         }
     }
 
@@ -70,27 +64,6 @@ public class AnimationTasks {
             }
             task.cancel();
         }
-    }
-
-    /**
-     * Convert Minecraft Ticks to milliseconds by getting the
-     * preferred tick interval {@link Scheduler#getPreferredTickInterval()}
-     * @param ticks - number of ticks
-     * @return - ticks in milliseconds
-     */
-    private long ticksToMilliseconds(long ticks) {
-        /*
-         * This has some downfalls... If a user starts an animation at a
-         * given time and lets it run for a while, wouldn't the preferred
-         * tick interval change? If so we would need to add functionality
-         * somewhere to keep track of the what the updated interval is in
-         * order for the delay between frames to be a consistent time and
-         * not changing because the preferred tick interval is changing.
-         * But wait... aren't the Frame Tasks synchronous? Meaning that
-         * they just happen every few ticks? (Look into this) TODO
-         */
-        Scheduler scheduler = Sponge.getScheduler();
-        return ticks * scheduler.getPreferredTickInterval();
     }
 
     /**
