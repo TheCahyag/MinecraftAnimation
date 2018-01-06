7e12c5c9f4886b0da2d69a13f99c099a3769bcb4
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-01-06 12:08:39 -0500

    Added Util static import and added a URL at the top of the help command directing to the github wiki

--- src/main/java/com/servegame/bl4de/Animation/command/animation/HelpAnimation.java
+++ src/main/java/com/servegame/bl4de/Animation/command/animation/HelpAnimation.java
@@ -1,6 +1,6 @@
 package com.servegame.bl4de.Animation.command.animation;
 
-import com.servegame.bl4de.Animation.util.Util;
+import com.servegame.bl4de.Animation.util.Resource;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -9,6 +9,11 @@ import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.servegame.bl4de.Animation.util.Util.*;
 
 /**
  * File: HelpAnimation.java
@@ -37,6 +42,11 @@ public class HelpAnimation implements CommandExecutor {
             /animate <name> frame update <name|num> -o
             ----------------------------------------------------
         */
+        URL url = null;
+        try {
+            url = new URL(Resource.COMMAND_URL);
+        } catch (MalformedURLException e){}
+
         Text message = Text.builder()
 //                .append(Text.of(TextColors.WHITE, "/"))
 //                .append(Text.builder()
@@ -44,102 +54,107 @@ public class HelpAnimation implements CommandExecutor {
 //                        .onClick(TextActions.runCommand("/animate"))
 //                        .onHover(Util.COMMAND_HOVER)
 //                        .build()) // Can't actually call this TODO
-                .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.ACTION_COLOR, " create",
-                        Util.NAME_COLOR, " <name>\n"))
-                .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.ACTION_COLOR, " delete",
-                        Util.NAME_COLOR, " <name>",
-                        Util.FLAG_COLOR, " -f\n"))
-                .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.ACTION_COLOR, " start",
-                        Util.NAME_COLOR, " <name>",
-                        Util.FLAG_COLOR, " -f <num> -d <num> -c <num>\n"))
-                .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.ACTION_COLOR, " stop",
-                        Util.NAME_COLOR, " <name>\n"))
-                .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.ACTION_COLOR, " pause",
-                        Util.NAME_COLOR, " <name>\n"))
-                .append(Text.of(TextColors.WHITE, "/"))
                 .append(Text.builder()
-                        .append(Text.of(Util.PRIMARY_COLOR, Util.COMMAND_STYLE, "animate", Util.ACTION_COLOR, " list\n"))
-                        .onClick(TextActions.runCommand("/animate list"))
-                        .onHover(Util.COMMAND_HOVER)
+                        .append(Text.of(PRIMARY_COLOR, COMMAND_STYLE, "For a full list of commands visit our wiki!\n"))
+                        .onClick(TextActions.openUrl(url))
+                        .onHover(TextActions.showText(Text.of("Click here to visit the wiki")))
                         .build())
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.ACTION_COLOR, " info\n"))
+                        PRIMARY_COLOR, "animate",
+                        ACTION_COLOR, " create",
+                        NAME_COLOR, " <name>\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.ACTION_COLOR, " set",
-                        Util.PRIMARY_COLOR, " <pos1|pos2> ",
-                        Util.FLAG_COLOR, "-f\n"))
+                        PRIMARY_COLOR, "animate",
+                        ACTION_COLOR, " delete",
+                        NAME_COLOR, " <name>",
+                        FLAG_COLOR, " -f\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.ACTION_COLOR, " set name",
-                        Util.NAME_COLOR, " <new_name> (Not impl.)\n"))
+                        PRIMARY_COLOR, "animate",
+                        ACTION_COLOR, " start",
+                        NAME_COLOR, " <name>",
+                        FLAG_COLOR, " -f <num> -d <num> -c <num>\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.ACTION_COLOR, " create",
-                        Util.NAME_COLOR, " <name>",
-                        Util.FLAG_COLOR, " -h\n"))
+                        PRIMARY_COLOR, "animate",
+                        ACTION_COLOR, " stop",
+                        NAME_COLOR, " <name>\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.ACTION_COLOR, " delete",
-                        Util.NAME_COLOR, " <name|num>",
-                        Util.FLAG_COLOR, " -f\n"))
+                        PRIMARY_COLOR, "animate",
+                        ACTION_COLOR, " pause",
+                        NAME_COLOR, " <name>\n"))
+                .append(Text.of(TextColors.WHITE, "/"))
+                .append(Text.builder()
+                        .append(Text.of(PRIMARY_COLOR, COMMAND_STYLE, "animate", ACTION_COLOR, " list\n"))
+                        .onClick(TextActions.runCommand("/animate list"))
+                        .onHover(COMMAND_HOVER)
+                        .build())
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.ACTION_COLOR, " display",
-                        Util.NAME_COLOR, " <name|num>\n"))
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        ACTION_COLOR, " info\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.ACTION_COLOR, " duplicate",
-                        Util.NAME_COLOR, " <name|num>",
-                        Util.PRIMARY_COLOR, " [num]\n"))
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        ACTION_COLOR, " set",
+                        PRIMARY_COLOR, " <pos1|pos2> ",
+                        FLAG_COLOR, "-f\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.ACTION_COLOR, " list\n"))
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        ACTION_COLOR, " set name",
+                        NAME_COLOR, " <new_name> (Not impl.)\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.ACTION_COLOR, " update",
-                        Util.NAME_COLOR, " <name|num>",
-                        Util.FLAG_COLOR, " -o\n"))
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        ACTION_COLOR, " create",
+                        NAME_COLOR, " <name>",
+                        FLAG_COLOR, " -h\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.NAME_COLOR, " <name|num>",
-                        Util.ACTION_COLOR, " info\n"))
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        ACTION_COLOR, " delete",
+                        NAME_COLOR, " <name|num>",
+                        FLAG_COLOR, " -f\n"))
                 .append(Text.of(TextColors.WHITE, "/",
-                        Util.PRIMARY_COLOR, "animate",
-                        Util.NAME_COLOR, " <name>",
-                        Util.PRIMARY_COLOR, " frame",
-                        Util.NAME_COLOR, " <name|num>",
-                        Util.ACTION_COLOR, " set name",
-                        Util.NAME_COLOR, " <new_name> (not impl)\n"))
-                .append(Text.of(Util.SECONDARY_COLOR, "----------------------------------------------------"))
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        ACTION_COLOR, " display",
+                        NAME_COLOR, " <name|num>\n"))
+                .append(Text.of(TextColors.WHITE, "/",
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        ACTION_COLOR, " duplicate",
+                        NAME_COLOR, " <name|num>",
+                        PRIMARY_COLOR, " [num]\n"))
+                .append(Text.of(TextColors.WHITE, "/",
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        ACTION_COLOR, " list\n"))
+                .append(Text.of(TextColors.WHITE, "/",
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        ACTION_COLOR, " update",
+                        NAME_COLOR, " <name|num>",
+                        FLAG_COLOR, " -o\n"))
+                .append(Text.of(TextColors.WHITE, "/",
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        NAME_COLOR, " <name|num>",
+                        ACTION_COLOR, " info\n"))
+                .append(Text.of(TextColors.WHITE, "/",
+                        PRIMARY_COLOR, "animate",
+                        NAME_COLOR, " <name>",
+                        PRIMARY_COLOR, " frame",
+                        NAME_COLOR, " <name|num>",
+                        ACTION_COLOR, " set name",
+                        NAME_COLOR, " <new_name> (not impl)\n"))
+                .append(Text.of(SECONDARY_COLOR, "----------------------------------------------------"))
                 .build();
         src.sendMessage(message);
         return CommandResult.success();
