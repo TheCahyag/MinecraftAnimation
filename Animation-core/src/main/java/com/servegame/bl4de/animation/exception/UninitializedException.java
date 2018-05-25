c8c8986ac7c576c29d40f1c58e976fcfe24b123f
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-05-25 18:35:08 -0400

    Created UninitializationException.java

---
+++ Animation-core/src/main/java/com/servegame/bl4de/animation/exception/UninitializedException.java
@@ -0,0 +1,9 @@
+package com.servegame.bl4de.animation.exception;
+
+/**
+ * File: UninitializationException.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class UninitializationException extends Exception {
+}
