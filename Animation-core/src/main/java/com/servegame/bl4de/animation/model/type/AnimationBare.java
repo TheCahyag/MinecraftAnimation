8a38e7e9e45bfa5b940fbf2700d276e79b454d1f
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-05-26 15:41:15 -0400

    Added state and stubbed out methods

--- Animation-core/src/main/java/com/servegame/bl4de/animation/model/type/AnimationBare.java
+++ Animation-core/src/main/java/com/servegame/bl4de/animation/model/type/AnimationBare.java
@@ -1,9 +1,52 @@
 package com.servegame.bl4de.animation.model.type;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * File: AnimationBare.java
  *
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class AnimationBare extends Animation {
+
+    /* State */
+
+    /** TODO **/
+    private List<String> frameNames;
+
+    /* Constructors */
+
+    /**
+     * TODO
+     */
+    public AnimationBare(){}
+
+    /**
+     * TODO
+     * @param owner TODO
+     * @param name TODO
+     */
+    public AnimationBare(UUID owner, String name) {
+        super(owner, name);
+        this.frameNames = new ArrayList<>();
+    }
+
+    /**
+     * TODO
+     * @param name TODO
+     */
+    public AnimationBare(String name) {
+        super(name);
+        this.frameNames = new ArrayList<>();
+    }
+
+    /* Methods */
+
+    @Override
+    public boolean isInit() {
+        // TODO
+        throw new RuntimeException();
+    }
 }
--- Animation-core/src/main/java/com/servegame/bl4de/animation/model/type/AnimationFull.java
+++ Animation-core/src/main/java/com/servegame/bl4de/animation/model/type/AnimationFull.java
@@ -1,15 +1,19 @@
 package com.servegame.bl4de.animation.model.type;
 
+import com.servegame.bl4de.animation.data.DataSerializable;
 import com.servegame.bl4de.animation.model.Frame;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * File: AnimationFull.java
  *
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
-public class AnimationFull {
+public class AnimationFull extends Animation implements DataSerializable {
 
     /* State */
 
@@ -18,7 +22,96 @@ public class AnimationFull {
 
     /* Constructors */
 
+    /**
+     * TODO
+     */
+    public AnimationFull(){}
+
+    /**
+     * TODO
+     * @param owner TODO
+     * @param name TODO
+     */
+    public AnimationFull(UUID owner, String name) {
+        super(owner, name);
+        this.frames = new ArrayList<>();
+    }
+
+    /**
+     * TODO
+     * @param name TODO
+     */
+    public AnimationFull(String name) {
+        super(name);
+        this.frames = new ArrayList<>();
+    }
+
     /* Methods */
+
+    @Override
+    public boolean isInit() {
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /**
+     * Gets a particular {@link Frame} at a given index
+     * @param frameNum index for frame array
+     * @return resulting {@link Frame}
+     */
+    public Optional<Frame> getFrame(int frameNum){
+        int numOfFrames = this.frames.size();
+        if (frameNum > numOfFrames || numOfFrames == 0 || frameNum < 0){
+            // Out of bounds
+            return Optional.empty();
+        } else {
+            return Optional.of(this.frames.get(frameNum));
+        }
+    }
+
+    /**
+     * Gets a particular named {@link Frame}
+     * @param frameName given string name
+     * @return given {@link Frame}
+     */
+    public Optional<Frame> getFrame(String frameName){
+        for (Frame frame :
+                this.frames) {
+            if (frame.getName().equals(frameName)) {
+                return Optional.of(frame);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /* START: Getters and Setters */
+
+    /**
+     * TODO
+     * @return TODO
+     */
+    public List<Frame> getFrames() {
+        return frames;
+    }
+
+    /**
+     * TODO
+     * @param frames TODO
+     */
+    public void setFrames(List<Frame> frames) {
+        this.frames = frames;
+    }
+
+    /**
+     * Get the number of frames contained in the {@link AnimationFull#frames} list
+     * @return int - number of frames currently assigned to this {@link Animation}
+     */
+    public List<Frame> getActiveFrames(){
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /* END: Getters and Setters */
 
 
 }
