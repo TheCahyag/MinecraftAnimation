527250dfbf6ba486a09e44938f987770d504a8ac
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-05-26 15:35:01 -0400

    Stubbed out static methods for the controllers

--- Animation-core/src/main/java/com/servegame/bl4de/animation/controller/AnimationBareController.java
+++ Animation-core/src/main/java/com/servegame/bl4de/animation/controller/AnimationBareController.java
@@ -1,9 +1,18 @@
 package com.servegame.bl4de.animation.controller;
 
+import com.servegame.bl4de.animation.model.type.AnimationFull;
+
 /**
  * File: AnimationBareController.java
  *
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public abstract class AnimationBareController {
+
+    public void getName(){
+        AnimationFull animationFull = new AnimationFull();
+        animationFull.get
+    }
+
+
 }
--- Animation-core/src/main/java/com/servegame/bl4de/animation/controller/AnimationFullController.java
+++ Animation-core/src/main/java/com/servegame/bl4de/animation/controller/AnimationFullController.java
@@ -1,9 +1,117 @@
 package com.servegame.bl4de.animation.controller;
 
+import com.servegame.bl4de.animation.exception.UninitializedException;
+import com.servegame.bl4de.animation.model.Frame;
+import com.servegame.bl4de.animation.model.type.Animation;
+import com.servegame.bl4de.animation.model.type.AnimationFull;
+
+import java.util.UUID;
+
 /**
  * File: AnimationFullController.java
  *
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public abstract class AnimationFullController {
+
+    /**
+     * TODO
+     * @param animation TODO
+     * @param owner TODO
+     * @return TODO
+     * @throws UninitializedException TODO
+     */
+    public static Frame getBlankFrame(AnimationFull animation, UUID owner, String name) throws UninitializedException {
+        // TODO
+        return null;
+    }
+
+    /**
+     * TODO
+     * @param animation TODO
+     * @param owner TODO
+     * @return TODO
+     * @throws UninitializedException TODO
+     */
+    public static Frame getBlankFrame(AnimationFull animation, UUID owner) throws UninitializedException {
+        // TODO
+        return null;
+    }
+
+    /**
+     * TODO
+     * @param animation TODO
+     * @param frame TODO
+     * @param index TODO
+     * @throws UninitializedException TODO
+     */
+    public static void addFrame(AnimationFull animation, Frame frame, int index) throws UninitializedException {
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /**
+     * TODO
+     * @param animation TODO
+     * @param frame TODO
+     * @throws UninitializedException TODO
+     */
+    public static void addFrame(AnimationFull animation, Frame frame) throws UninitializedException {
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /* START: Action Methods */
+
+    /**
+     * TODO
+     * @param animation TODO
+     * @param frame TODO
+     * @throws UninitializedException TODO
+     */
+    public static boolean start(AnimationFull animation, int frame) throws UninitializedException {
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /**
+     * TODO
+     * @param animation TODO
+     * @throws UninitializedException TODO
+     */
+    public static boolean start(AnimationFull animation) throws UninitializedException {
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /**
+     * TODO
+     */
+    public static boolean stop(){
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /**
+     * TODO
+     */
+    public static boolean pause(){
+        // TODO
+        throw new RuntimeException();
+    }
+
+    /* END: Action Methods */
+
+    /* START: Frame Manipulation */
+
+    /**
+     * TODO
+     * @param animation TODO
+     * @param frame TODO
+     */
+    public void deleteFrame(AnimationFull animation, Frame frame){
+        // TODO
+        throw new RuntimeException();
+    }
+
 }
