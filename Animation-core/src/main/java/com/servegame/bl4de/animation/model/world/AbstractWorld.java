77ef17f3b74ffcdfdcae8abf342bb667dcf36bca
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-05-25 21:44:30 -0400

    Moved DataSerializable.java from the model package to the model.world package

---
+++ Animation-core/src/main/java/com/servegame/bl4de/animation/model/world/AbstractWorld.java
@@ -0,0 +1,9 @@
+package com.servegame.bl4de.animation.model.world;
+
+/**
+ * File: AbstractWorld.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class AbstractWorld {
+}
---
+++ Animation-core/src/main/java/com/servegame/bl4de/animation/model/world/World.java
@@ -0,0 +1,9 @@
+package com.servegame.bl4de.animation.model.world;
+
+/**
+ * File: World.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public interface World {
+}
